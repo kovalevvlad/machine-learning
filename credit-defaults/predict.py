04ac75329f2df80a4a3b17f3f1f22a340d9df0ce
Vladimir Kovalev <kovalev.vladimir1990@gmail.com>
2017-04-21 15:03:11 +0800

    Switch to F1 scoring. Add a new classifier for comparison.

--- credit-defaults/predict.py
+++ credit-defaults/predict.py
@@ -1,7 +1,8 @@
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import LinearSVC
 from data import X, y
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import classification_report
 import numpy as np
 
@@ -11,11 +12,21 @@ scaler.fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-parameters = {
-    'alpha': 10.0 ** -np.arange(1, 7),
-    'loss': ['hinge', 'log']
+params = {'scoring': 'f1', 'n_jobs': -1}
+
+clfs = {
+    'sgd': GridSearchCV(
+        SGDClassifier(),
+        param_grid={'alpha': 10.0 ** -np.arange(1, 12), 'loss': ['hinge', 'log']},
+        **params),
+
+    'svc': GridSearchCV(LinearSVC(), param_grid={'C': 10.0 ** np.arange(-2, 10)}, **params)
 }
 
-clf = GridSearchCV(SGDClassifier(), param_grid=parameters, n_jobs=-1)
-best_model = clf.fit(X_train_scaled, y_train)
-print classification_report(y_test, best_model.predict(X_test))
+fitted_clfs = {name: clf.fit(X_train_scaled, y_train) for name, clf in clfs.items()}
+
+for name, fitted_clf in fitted_clfs.items():
+    print "{} stats".format(name)
+    print "params: {}".format(fitted_clf.best_params_)
+    print classification_report(y_test, fitted_clf.predict(X_test_scaled))
+    print ""
