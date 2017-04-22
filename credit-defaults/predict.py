b9eaad82841950d208bbd6155e981ed7ba112158
Vladimir Kovalev <kovalev.vladimir1990@gmail.com>
2017-04-22 15:46:29 +0800

    Adding SVC RBF and KNN classifiers

--- credit-defaults/predict.py
+++ credit-defaults/predict.py
@@ -1,10 +1,14 @@
 from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+
 from data import X, y
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
 import numpy as np
+from debug import plot_learning_curve
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 scaler = StandardScaler()
@@ -15,18 +19,18 @@ X_test_scaled = scaler.transform(X_test)
 params = {'scoring': 'f1', 'n_jobs': -1}
 
 clfs = {
-    'sgd': GridSearchCV(
-        SGDClassifier(),
-        param_grid={'alpha': 10.0 ** -np.arange(1, 12), 'loss': ['hinge', 'log']},
-        **params),
-
-    'svc': GridSearchCV(LinearSVC(), param_grid={'C': 10.0 ** np.arange(-2, 10)}, **params)
+    'sgd': SGDClassifier(alpha=1e-9, loss='log'),
+    'svc': LinearSVC(C=10),
+    'svc rbf': SVC(C=100, gamma=0.01),
+    'KNN': KNeighborsClassifier(n_neighbors=5, weights='distance')
 }
 
 fitted_clfs = {name: clf.fit(X_train_scaled, y_train) for name, clf in clfs.items()}
 
 for name, fitted_clf in fitted_clfs.items():
     print "{} stats".format(name)
-    print "params: {}".format(fitted_clf.best_params_)
+    print "Test Report"
     print classification_report(y_test, fitted_clf.predict(X_test_scaled))
-    print ""
+    print "Training Report"
+    print classification_report(y_train, fitted_clf.predict(X_train_scaled))
+    plot_learning_curve(fitted_clf, name, X_train_scaled, y_train, **params).show()
